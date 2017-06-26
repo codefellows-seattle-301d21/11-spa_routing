8f5e759da401c2be09a5838639b30680bdba3ff0
ohjonah <ohjonah@gmail.com>
2017-06-26 16:00:21 -0700

    Routes Client Side
    Routes client side using page.js

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://localhost/5432/kilovolt'; // DONE: Don't forget to set your own conString
+const conString = 'postgres://localhost:5432/kilovolt'; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
@@ -21,7 +21,7 @@ app.get('/admin', (request, response) => response.sendFile('admin.html', {root: 
 app.get('/articles', (request, response) => {
   client.query(`
     SELECT * FROM articles
-    INNER JOIN author
+    INNER JOIN authors
       ON articles.author_id=authors.author_id;`
   )
   .then(result => response.send(result.rows))
