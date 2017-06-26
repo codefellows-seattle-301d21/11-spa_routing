a426188ed11a4e18cfff5da5b119e0c43a150516
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-26 16:58:56 -0700

    Added page.js routes an debugged
    There was some naming conventio errors and typos.
    But it's all good in the hood now :D

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -4,7 +4,7 @@ var app = app || {};
 (function(module) {
   const articleController = {};
   articleController.fetchArticles = () => {
-    app.Articles.fetchAll(app.articleView.initIndexPage);
+    app.Article.fetchAll(app.articleView.initIndexPage);
   };
 
 
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -2,6 +2,6 @@
 var app = app || {};
 
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
-page('/', app.aboutController.about);
-page('articles', app.articleController.fetchArticles);
+page('/about', app.aboutController.about);
+page('/', app.articleController.fetchArticles);
 page();
