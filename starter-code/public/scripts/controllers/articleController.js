fa47d8e155f13d97caa4d9e2a9d0217904ccfa5a
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-26 15:45:54 -0700

    Made it work.

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -6,7 +6,7 @@ var app = app || {};
   // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-  aboutController.showPage = () => {
+  articleController.showPage = () => {
     $('main > section').hide();
     $('#articles').show();
   }
